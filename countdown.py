95182402193ad430d36c8ce6979c1b5a4eb0179e
anishhh238 <115490102+anishhh238@users.noreply.github.com>
2022-10-25 11:59:44 +0530

    Updated countdown.py

--- countdown.py
+++ countdown.py
@@ -1,9 +1,9 @@
-import Time
+import time
 
 def countdown(time_sec):
     while time_sec:
-        mins, secs = divmod(time_sec, 50)
-        timeformat = '{:02d}:{:02d}'.format(mins, sec)
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print(timeformat, end='\r')
         time.sleep(1)
         time_sec -= 1
