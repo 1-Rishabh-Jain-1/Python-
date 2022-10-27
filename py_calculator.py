41d55df78f0e84203541841400f5df4ab605cdc3
Rishabh Jain <73517462+1-Rishabh-Jain-1@users.noreply.github.com>
2022-10-27 19:57:09 +0530

    Update py_calculator.py

--- py_calculator.py
+++ py_calculator.py
@@ -16,19 +16,24 @@ def multiply(x, y):
 def divide(x, y):
     return x / y # added ' / ' operator to divide the two input numbers
 
+# This function raises a number to the others power
+def pow(x, y):
+    return x ** y
+
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponent")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("Enter first number: ")) # added input to take num1 as float input 
         num2 = float(input("Enter second number: ")) # added input to take num2 as float input 
 
@@ -43,6 +48,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+        
+        elif choice == '5':
+            print(num1, "^", num2, "=", pow(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
